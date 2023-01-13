862253bb642bd032be8aaaa4dc7a26363c0b2745
Christos Galanopoulos <xrholy11@gmail.com>
2023-01-13 14:45:40 +0200

    Update content

--- .github/scripts/create_content_label.sh
+++ .github/scripts/create_content_label.sh
@@ -3,9 +3,9 @@
 # Fetch all repositories and keep only their full names (user/repo)
 repos=$(curl \
     -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer $TOKEN" \
+    -H "Authorization: Bearer ${{ secrets.ISSUE_MANAGER_PAT }}" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
-    https://api.github.com/user/repos | jq -r '.[].full_name')
+https://api.github.com/user/repos | jq -r '.[].full_name')
 
 
 # Create a content label to every repository
@@ -15,7 +15,7 @@ do
     curl \
     -X POST \
     -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer $TOKEN"\
+    -H "Authorization: Bearer ${{ secrets.ISSUE_MANAGER_PAT }}"\
     -H "X-GitHub-Api-Version: 2022-11-28" \
     https://api.github.com/repos/$repo/labels \
     -d '{"name":"content","description":"Cloud-on-Rails related","color":"0052cc"}'
