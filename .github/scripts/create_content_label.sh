4dce56fb156bde91dfd4407d4653b6ec2f2985c8
Christos Galanopoulos <xrholy11@gmail.com>
2023-01-13 14:55:03 +0200

    Update content

--- .github/scripts/create_content_label.sh
+++ .github/scripts/create_content_label.sh
@@ -11,7 +11,7 @@ https://api.github.com/user/repos | jq -r '.[].full_name')
 # Create a content label to every repository
 for repo in $repos;
 do
-    echo "Creating content label to $repo ..."
+    printf "Creating content label to $repo ...\n"
     curl \
     -X POST \
     -H "Accept: application/vnd.github+json" \
@@ -19,4 +19,5 @@ do
     -H "X-GitHub-Api-Version: 2022-11-28" \
     https://api.github.com/repos/$repo/labels \
     -d '{"name":"content","description":"Cloud-on-Rails related","color":"0052cc"}'
+    printf "\n\n"
 done
