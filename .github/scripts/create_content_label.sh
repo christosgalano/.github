f656a6b30b2a72ebf7e6f2df18f9643a2d91bc72
Christos Galanopoulos <xrholy11@gmail.com>
2023-01-13 15:07:15 +0200

    Update content

--- .github/scripts/create_content_label.sh
+++ .github/scripts/create_content_label.sh
@@ -15,10 +15,9 @@ do
     curl \
     -X POST \
     -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer $1"\
+    -H "Authorization: Bearer $1" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
-    -s -w 'code: %{response_code}' \
     https://api.github.com/repos/$repo/labels \
-    -d '{"name":"content","description":"Cloud-on-Rails related","color":"0052cc"}'
+    -s -d '{"name":"content","description":"Cloud-on-Rails related","color":"0052cc"}'
     printf "\n\n"
 done
