0e169d75230a2fca479634454ddc4f3da8020e2c
Christos Galanopoulos <xrholy11@gmail.com>
2023-01-13 15:08:36 +0200

    Update content

--- .github/scripts/create_content_label.sh
+++ .github/scripts/create_content_label.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Fetch all repositories and keep only their full names (user/repo)
-repos=$(curl \
+repos=$(curl -s \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer $1" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
